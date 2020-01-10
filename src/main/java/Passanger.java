8a60d750260474ad96ff7f1a86079e9e19650483
Catrin <catrinhughes92@gmail.com>
2020-01-10 12:18:34 +0000

    can get passanger name

--- src/main/java/Passanger.java
+++ src/main/java/Passanger.java
@@ -1,2 +1,14 @@
 public class Passanger {
+
+    private String name;
+    private int bags;
+
+    public Passanger(String name, int bags) {
+        this.name = name;
+        this.bags = bags;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
--- src/test/java/PassangerTest.java
+++ src/test/java/PassangerTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassangerTest {
+
+    private Passanger passanger;
+
+    @Before
+    public void before() {
+        passanger = new Passanger("Eugene Kim", 2);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Eugene Kim", passanger.getName());
+    }
 }
